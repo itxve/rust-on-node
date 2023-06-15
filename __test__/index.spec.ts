a5af8b3d5bcdc9a4839ccd2bd4c29006666bab66
tianxueren <1770664399@qq.com>
2023-06-15 19:22:39 +0800

    成功 ...

--- __test__/index.spec.ts
+++ __test__/index.spec.ts
@@ -3,13 +3,15 @@ import test from 'ava'
 import { asyncPlus100, asyncFib, updateAgdvt } from '../index'
 
 test('[Plus 100 sleep]: from native', async (t) => {
-  const content = await asyncPlus100(Promise.resolve(10))
-  t.is(content, 110)
+  asyncPlus100(Promise.resolve(10)).then((content) => {
+    t.is(content, 110)
+  })
 })
 
-test('[AsyncFib]', async (t) => {
-  const content = await asyncFib(12)
-  t.is(content, 13)
+test('[AsyncFib]', (t) => {
+  asyncFib(12).then((content) => {
+    t.is(content, 13)
+  })
 })
 
 test('[UpdateAgdvt]', (t) => {
