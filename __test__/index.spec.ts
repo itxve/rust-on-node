3cd439d6934d8232ad93f691b025f8f5851724a5
tianxueren <1770664399@qq.com>
2023-06-15 18:49:11 +0800

    test build

--- __test__/index.spec.ts
+++ __test__/index.spec.ts
@@ -1,8 +1,28 @@
 import test from 'ava'
 
-import { plus100 } from '../index'
+import { readFileAsync, asyncPlus100, asyncFib, updateAgdvt } from '../index'
 
-test('sync function from native code', (t) => {
-  const fixture = 42
-  t.is(plus100(fixture), fixture + 100)
+test('[Read File]: from native', async (t) => {
+  const content = await readFileAsync('src/ff.rs')
+  t.assert(!!content)
+})
+
+test('[Plus 100 sleep]: from native', async (t) => {
+  const content = await asyncPlus100(Promise.resolve(10))
+  t.is(content, 110)
+})
+
+test('[AsyncFib]', async (t) => {
+  const content = await asyncFib(12)
+  t.is(content, 13)
+})
+
+test('[UpdateAgdvt]', (t) => {
+  const content = updateAgdvt(
+    {
+      sgt: '23',
+    },
+    '100',
+  )
+  t.assert(content.sgt, '100')
 })
--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,61 @@
 
 use napi_derive::napi;
 
+use core::time;
+use futures::prelude::*;
+use napi::bindgen_prelude::*;
+use napi::*;
+use tokio::fs;
+
 #[napi]
-pub fn plus_100(input: u32) -> u32 {
-  input + 100- 10+10
+pub async fn read_file_async(path: String) -> Result<Buffer> {
+  fs::read(path)
+    .map(|r| match r {
+      Ok(content) => Ok(content.into()),
+      Err(e) => Err(Error::new(
+        Status::GenericFailure,
+        format!("failed to read file, {}", e),
+      )),
+    })
+    .await
+}
+
+#[napi]
+pub async fn async_plus_100(p: Promise<u32>) -> Result<u32> {
+  let v = p.await?;
+  std::thread::sleep(time::Duration::from_secs(5));
+  Ok(v + 100)
+}
+
+struct PlusOneAsync(u32);
+
+#[napi]
+impl Task for PlusOneAsync {
+  type Output = u32;
+  type JsValue = JsNumber;
+
+  fn compute(&mut self) -> Result<Self::Output> {
+    std::thread::sleep(time::Duration::from_secs(2));
+    Ok(self.0 + 1)
+  }
+
+  fn resolve(&mut self, env: Env, output: Self::Output) -> Result<Self::JsValue> {
+    env.create_uint32(output)
+  }
+}
+
+#[napi]
+fn async_fib(input: u32) -> AsyncTask<PlusOneAsync> {
+  AsyncTask::new(PlusOneAsync(input))
+}
+
+#[napi(object)]
+struct Agdvt {
+  pub sgt: Option<String>,
+}
+
+#[napi]
+fn update_agdvt(mut agt: Agdvt, gh: String) -> Result<Agdvt> {
+  agt.sgt = Some(gh);
+  Ok(agt)
 }
